d68c8530e00e1fbf570ea6aee9cbd68e10f54e28
Greyno <gamrey@mat-mac.com>
2021-12-21 15:40:58 -0500

    Starts to add ability to create new objects on the main view, loading from UserDefaults

--- Samples_PassData/User.swift
+++ Samples_PassData/User.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 class User: ObservableObject {
 
-    @Published var firsts = ["Jane", "Bob", "Henry", "Susan"]
+    //@Published var firsts = ["Jane", "Bob", "Henry", "Susan"]
     @Published var lasts = ["Doe", "Smith", "Brown", "Headley"]
+    
+    @Published var firsts = [String]() {
+        didSet {
+            guard let data = try? JSONEncoder().encode(firsts.self) else { return }
+            UserDefaults.standard.set(data, forKey: "UserStore")
+        }
+    }
+    
+    init() {
+        if let firsts = UserDefaults.standard.data(forKey: "UserStore") {
+            if let decoded = try? JSONDecoder().decode([String].self, from: firsts) {
+                self.firsts = decoded
+                return
+            }
+        }
+        self.firsts = []
+        }
    
 }
