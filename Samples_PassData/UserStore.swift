a3941e628fc8bcb207722c2b848ef2239a5b18e8
Greyno <gamrey@mat-mac.com>
2021-12-23 13:42:20 -0500

    Code clean-up

--- Samples_PassData/UserStore.swift
+++ Samples_PassData/UserStore.swift
@@ -10,23 +10,7 @@ import SwiftUI
 class UserStore: ObservableObject {
     
     var user = User(firstName: "", lastName: "")
-    
-    //@Published var lasts = ["Doe", "Smith", "Brown", "Headley"]
-    
-//    @Published var lasts = [String]() {
-//        didSet {
-//            guard let data = try? JSONEncoder().encode(lasts.self) else { return }
-//            UserDefaults.standard.set(data, forKey: "Lasts")
-//        }
-//    }
-//    
-//    @Published var firsts = [String]() {
-//        didSet {
-//            guard let data = try? JSONEncoder().encode(firsts.self) else { return }
-//            UserDefaults.standard.set(data, forKey: "Firsts")
-//        }
-//    }
-    
+
     @Published var users = [User]() {
         didSet {
             guard let data = try? JSONEncoder().encode(users.self) else { return }
@@ -35,19 +19,6 @@ class UserStore: ObservableObject {
     }
     
     init() {
-//        if let firsts = UserDefaults.standard.data(forKey: "Firsts") {
-//            if let decoded = try? JSONDecoder().decode([String].self, from: firsts) {
-//                self.firsts = decoded
-//                return
-//            }
-//        }
-//
-//        if let lasts = UserDefaults.standard.data(forKey: "Lasts") {
-//            if let decoded = try? JSONDecoder().decode([String].self, from: lasts) {
-//                self.lasts = decoded
-//                return
-//            }
-//        }
         
         if let users = UserDefaults.standard.data(forKey: "Users") {
             if let decoded = try? JSONDecoder().decode([User].self, from: users) {
@@ -55,8 +26,6 @@ class UserStore: ObservableObject {
                 return
             }
         }
-//        self.firsts = []
-//        self.lasts = []
         self.users = []
         }
 
@@ -67,14 +36,10 @@ class UserStore: ObservableObject {
     
     func addUser(firstName: String, lastName: String) {
         self.user = User(firstName: firstName, lastName: lastName)
-        //firsts.append(user.firstName)
-        //lasts.append(user.lastName)
         users.append(user)
     }
     
     func deleteUser(at offsets: IndexSet) {
-//        firsts.remove(atOffsets: offsets)
-//        lasts.remove(atOffsets: offsets)
         users.remove(atOffsets: offsets)
     }
    
